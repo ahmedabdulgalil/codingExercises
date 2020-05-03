0f144bbd17bda0a2bbd704fdf5de3ccae4085c4d
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-05-02 22:11:38 -0400

    LeapYear Challenge

--- codingExercises/src/Main.java
+++ codingExercises/src/Main.java
@@ -3,10 +3,19 @@ public class Main {
 	
 	public static void main(String [] args) {
 		
-		SpeedConverter.printConversion(10.5);
+//		SpeedConverter.printConversion(10.5);
+//		
+//		long miles = SpeedConverter.toMilesPerHour(10.5);
+//		System.out.println( miles + " Miles");
 		
-		long miles = SpeedConverter.toMilesPerHour(10.5);
-		System.out.println( miles + " Miles");
+//		MegaBytesConverter.printMegaBytesAndKiloBytes(-120);
+		
+		
+	 boolean year = LeapYear.isLeapYear(1800);
+	 
+	 System.out.println(year);
+		
+
 	}
 
 
