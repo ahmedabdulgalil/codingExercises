b75f5d6e80694f21b60811274269188c2b2e5e02
Abdul-Mac <Abdul-Mac@abdulgalils-mbp>
2020-05-06 00:02:24 -0400

    feetandInches challenge

--- codingExercises/src/Main.java
+++ codingExercises/src/Main.java
@@ -2,21 +2,14 @@
 public class Main {
 	
 	public static void main(String [] args) {
-		
-//		SpeedConverter.printConversion(10.5);
-//		
-//		long miles = SpeedConverter.toMilesPerHour(10.5);
-//		System.out.println( miles + " Miles");
-		
-//		MegaBytesConverter.printMegaBytesAndKiloBytes(-120);
+	
+		FeetAndInches.calcFeetAndInchesToCentimeters(6, 0);
 		
 		
-	 boolean decimal = EqualSumChecker.hasEqualSum(-1, -1, 0);
-	 
-	 System.out.println(decimal);
 		
-
+		FeetAndInches.calcFeetAndInchesToCentimeters(40);
+	
 	}
-
+	
 
 }
